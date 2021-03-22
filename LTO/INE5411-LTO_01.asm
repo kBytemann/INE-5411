9781025653b5ea406ff3be97572a2f8df99a614b
Alexandre Genthner <43814465+kBytemann@users.noreply.github.com>
2021-03-22 18:12:25 -0300

    Delete INE5411-LTO_01.asm

--- LTO/INE5411-LTO_01.asm
+++
@@ -1,6 +0,0 @@
-.text
-.globl main
-main:
-	add $s0, $zero, $gp	#carrega em s0 o endereço de gp
-	lbu $t0, 0($s0)		#carrega o valor do byte menos significativo apontado por s0 em t0
-	sb $t1, 1($s0)		# sobreescreve no endereço de s0 o valor do segundo byte menos significativo
