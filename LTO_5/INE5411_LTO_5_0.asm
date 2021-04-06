054303299d1eedd02c4e6613ec9da9ed8255a729
Alexandre Genthner <43814465+kBytemann@users.noreply.github.com>
2021-04-06 18:46:19 -0300

    Rename INE5411_LTO_5_original.asm to INE5411_LTO_5_0.asm

--- LTO_5/INE5411_LTO_5_0.asm
+++ LTO_5/INE5411_LTO_5_0.asm
@@ -21,9 +21,9 @@ sort:
   sw   $s0,0($sp)  
 
 
-  move $s0,$zero   # MARCA 0: inicializaÁ„o da vari·vel i
+  move $s0,$zero   # MARCA 0: inicializa√ß√£o da vari√°vel i
 
-# inÌcio do corpo do laÁo externo
+# in√≠cio do corpo do la√ßo externo
 for1tst:  
 
   nop # MARCA 1
@@ -31,7 +31,7 @@ for1tst:
   beq  $t0,$zero,exit1  
   addi $s1,$s0,-1  
 
-# inicio do corpo do laÁo interno
+# inicio do corpo do la√ßo interno
 for2tst:
   slti $t0,$s1,0  
   bne  $t0,$zero,exit2 
@@ -47,18 +47,18 @@ for2tst:
   jal  swap 	
   addi $s1,$s1,-1
   j    for2tst
-# fim do corpo do laÁo interno
+# fim do corpo do la√ßo interno
 exit2:
   addi $s0,$s0,1
   j    for1tst
-# fim do corpo do laÁo externo
+# fim do corpo do la√ßo externo
 exit1:
   lw   $s0,0($sp)  
   lw   $s1,4($sp)
   lw   $ra,8($sp)
   addi $sp,$sp,12
   jr   $ra
-# implementaÁ„o da procedure swap
+# implementa√ß√£o da procedure swap
 swap:
   sll  $t1,$a1,2   # reg $t1=k+4
   add  $t1,$a0,$t1 # reg $t1=v+(k*4)
